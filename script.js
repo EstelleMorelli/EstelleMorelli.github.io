345a3b18e0ad5ec3a927fe9f0f799adee65fdad6
EstelleMorelli <estelle.morelli@oclock.school>
2024-02-15 13:14:22 +0100

    ajout de 2 autres éléments, il reste du boulot pour le contenu mais les animations sont OK, changer le background aussi

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
 window.addEventListener("load",function(){
     console.log("coucou je suis dans la console")
     var PC = document.getElementById("pc")
-    var encart = document.getElementById("gout_dev")
-    var button = document.querySelector(".closing")
+    var competences = document.getElementById("gout_dev")
+    var button1 = document.querySelector(".closingpc")
+    
     PC.addEventListener("click",function(){
-        encart.style.display = "block"
+        competences.style.display = "block"
     })
-    button.addEventListener("click",function(){
-        encart.style.display = "none"
+    button1.addEventListener("click",function(){
+        competences.style.display = "none"
     })
+
+    var radio = document.getElementById("radio")
+    var musique = document.getElementById("music")
+    var button2 = document.querySelector(".closingradio")
+
+    radio.addEventListener("click",function(){
+        musique.style.display = "block"
+    })
+    button2.addEventListener("click",function(){
+        musique.style.display = "none"
+    })
+
+    var book = document.getElementById("book")
+    var aboutme = document.getElementById("aboutme")
+    var button3 = document.querySelector(".closingbook")
+    
+    book.addEventListener("click",function(){
+        aboutme.style.display = "block"
+    })
+    button3.addEventListener("click",function(){
+        aboutme.style.display = "none"
+    })
+
+    
+    
 })
