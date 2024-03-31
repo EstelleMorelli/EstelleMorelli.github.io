2a3d6bc12afcf6aaa722269f279cd0e3addcce09
EstelleMorelli <estelle.morelli@oclock.school>
2024-03-31 23:13:04 +0200

    avec le lecteur musique c'est extra

--- script2.js
+++ script2.js
@@ -1,36 +1,54 @@
 const buttons = {
 
-    init: function(){
-        
-        for (const button of document.querySelectorAll(".closingButton")){
+    init: function () {
+
+        for (const button of document.querySelectorAll(".closingButton")) {
             button.addEventListener("click", buttons.handleDisplayParent);
         }
-        for (const cImg of document.querySelectorAll(".clickImg")){
+        for (const cImg of document.querySelectorAll(".clickImg")) {
             cImg.addEventListener("click", buttons.handleDisplayChild);
         }
+        buttons.handleMusic();
     },
 
-    handleDisplayParent: function(event){
+    handleDisplayParent: function (event) {
 
         buttons.changeDisplayParent(event.currentTarget);
     },
-    handleDisplayChild: function(event){
+    handleDisplayChild: function (event) {
 
         buttons.changeDisplayChild(event.currentTarget);
-        
+
     },
 
-    changeDisplayParent: function(elementDisplay){
+    changeDisplayParent: function (elementDisplay) {
         console.log(elementDisplay.parentNode);
         elementDisplay.parentNode.style.display = "none";
-        console.log(elementDisplay.parentNode.style.display);    
+        console.log(elementDisplay.parentNode.style.display);
     },
-    
-    changeDisplayChild: function(elementDisplay){
+
+    changeDisplayChild: function (elementDisplay) {
         elementDisplay.nextElementSibling.style.display = "block";
     },
+
+    handleMusic: function () {
+        // Récupérer l'élément image et l'audio
+        var playButton = document.getElementById("radio");
+        var audio = document.getElementById("myAudio");
+
+        // Fonction pour jouer ou mettre en pause la musique
+        function toggleAudio() {
+            if (audio.paused) {
+                audio.play();
+            } else {
+                audio.pause();
+            }
+        }
+
+        // Ajouter un événement de clic à l'image
+        playButton.addEventListener("click", toggleAudio);
+
+    },
 };
-
-
 
 document.addEventListener("DOMContentLoaded", buttons.init);
