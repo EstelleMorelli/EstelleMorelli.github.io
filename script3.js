3a25e5259e81bc8f4e59733ef670443be219fb0f
EstelleMorelli <estelle.morelli@oclock.school>
2024-04-03 13:43:32 +0200

    j'ai viré ces foutues étoiles

--- script3.js
+++ script3.js
@@ -1,35 +1,20 @@
-const star = {
+const radioEvent = {
   body: document.querySelector('body'),
   radio: document.querySelector('#radio'),
 
   init: function () {
-    star.radio.addEventListener("click", star.radioToggle);
+    radioEvent.radio.addEventListener("click", radioEvent.radioToggle);
   },
 
   radioToggle: function () {
-    star.radio.classList.toggle("on");
-    if (star.radio.classList.contains("on")) {
+    radioEvent.radio.classList.toggle("on");
+    if (radioEvent.radio.classList.contains("on")) {
       //document.querySelector(".scripts").innerHTML = '<script class="script3" src="script3.js"></script>';
       document.querySelector(".styles").innerHTML ='<link class="style2" href="style2.css" rel="stylesheet">';
-      star.mouseStar();
-    } else {
+      } else {
       document.querySelector(".style2").remove();
      }
   },
-
-  mouseStar: function () {
-    document.addEventListener("mousemove", function (e) {
-      let note = document.createElement("div");
-      note.className = "note";
-      note.style.left = e.pageX + "px";
-      note.style.top = e.pageY + "px";
-      document.body.appendChild(note);
-
-      setTimeout(function () {
-        note.remove();
-      }, 1000); 
-    }, 200);
-  },
 };
 
-document.addEventListener("DOMContentLoaded", star.init);
+document.addEventListener("DOMContentLoaded", radioEvent.init);
